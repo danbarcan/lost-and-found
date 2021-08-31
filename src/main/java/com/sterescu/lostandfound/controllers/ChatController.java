f4e6113d189bc6fc4d9d914aeb5ca3699d2ede02
Dan Barcan <dan.barcan1994@gmail.com>
2021-08-31 20:32:45 +0300

    Revert "remove cross origin from controller"
    This reverts commit 88fdeba7

--- src/main/java/com/sterescu/lostandfound/controllers/ChatController.java
+++ src/main/java/com/sterescu/lostandfound/controllers/ChatController.java
@@ -23,30 +23,35 @@ public class ChatController {
         this.chatService = chatService;
     }
 
+    @CrossOrigin(origins = "http://localhost:5001")
     @PostMapping("/chat")
     //@PreAuthorize("hasRole('USER') OR hasRole('ADMIN') OR hasRole('SERVICE')")
     public ResponseEntity<List<ChatMessage>> postMessage(@Valid @RequestBody ChatMessagePayload chatMessagePayload) {
         return chatService.postMessage(chatMessagePayload);
     }
 
+    @CrossOrigin(origins = "http://localhost:5001")
     @GetMapping("/chat/{itemId}")
     //@PreAuthorize("hasRole('USER') OR hasRole('ADMIN') OR hasRole('SERVICE')")
     public ResponseEntity<List<ChatMessage>> getMessagesByJobId(@PathVariable(value = "itemId") Long itemId) {
         return chatService.getMessagesByJobId(itemId);
     }
 
+    @CrossOrigin(origins = "http://localhost:5001")
     @GetMapping("/chat/unread")
     //@PreAuthorize("hasRole('USER') OR hasRole('ADMIN') OR hasRole('SERVICE')")
     public ResponseEntity<Map<Long, Long>> getUnreadMessagesByJob() {
         return chatService.getUnreadMessagesByJob();
     }
 
+    @CrossOrigin(origins = "http://localhost:5001")
     @GetMapping("/chat")
     //@PreAuthorize("hasRole('USER') OR hasRole('ADMIN') OR hasRole('SERVICE')")
     public ResponseEntity<Map<Long, List<ChatMessage>>> getMessagesGroupByJob() {
         return chatService.getMessagesGroupByJob();
     }
 
+    @CrossOrigin(origins = "http://localhost:5001")
     @GetMapping("/chat/items")
     //@PreAuthorize("hasRole('USER') OR hasRole('ADMIN') OR hasRole('SERVICE')")
     public ResponseEntity<List<Item>> getAllJobsWithMessages() {
--- src/main/java/com/sterescu/lostandfound/controllers/ItemController.java
+++ src/main/java/com/sterescu/lostandfound/controllers/ItemController.java
@@ -21,21 +21,25 @@ public class ItemController {
         this.itemService = itemService;
     }
 
+    @CrossOrigin(origins = "http://localhost:5001")
     @GetMapping("/item")
     public ResponseEntity<List<ItemResponsePayload>> getAllItems() {
         return itemService.getAllItems();
     }
 
+    @CrossOrigin(origins = "http://localhost:5001")
     @DeleteMapping("/item/{id}")
     public ResponseEntity<List<ItemResponsePayload>> deleteItem(@PathVariable Long id) {
         return itemService.deleteItem(id);
     }
 
+    @CrossOrigin(origins = "http://localhost:5001")
     @PostMapping("/item")
     public ResponseEntity<List<ItemResponsePayload>> saveItem(@Valid @ModelAttribute ItemPayload itemPayload) throws IOException {
         return itemService.saveItem(itemPayload);
     }
 
+    @CrossOrigin(origins = "http://localhost:5001")
     @PutMapping("/item/{id}")
     public ResponseEntity<List<ItemResponsePayload>> updateItem(@PathVariable Long id, @Valid @ModelAttribute ItemPayload itemPayload) throws IOException {
         return itemService.updateItem(id, itemPayload);
